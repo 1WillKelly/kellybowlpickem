fed9ad87d89a8f308e0ed6bb65854b6da393bf65
Aiden Scandella <sc@ndella.com>
2022-12-09 14:29:53 -0800

    Add CFB datasource and return from /api/examples (#5)
    * Add CFB datasource and return from /api/examples
    
    * Silence vercel
    
    * Remove console.log

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -1,10 +1,16 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
-import { prisma } from "../../server/db/client";
+import { CFBDataSource } from "../../server/datasources/college-football-data-api";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const examples = await prisma.example.findMany();
-  res.status(200).json(examples);
+  const client = new CFBDataSource();
+  const huskyStats = await client.makeRequest("/stats/season", {
+    year: 2022,
+    team: "Washington",
+  });
+  res.status(200).json({
+    huskyStats,
+  });
 };
 
 export default examples;
---
+++ src/server/datasources/college-football-data-api.ts
@@ -0,0 +1,40 @@
+import fetch from "cross-fetch";
+
+import { env } from "../../env/server.mjs";
+const CFB_ENDPOINT = "https://api.collegefootballdata.com";
+
+interface RequestOptions {
+  [key: string]: string | number;
+}
+
+export class CFBDataSource {
+  private auth: string;
+
+  constructor() {
+    this.auth = `Bearer ${env.CFBD_API_KEY}`;
+  }
+
+  // HACK: CFBD doesn't support `+` in params, and needs `%20`
+  private urlEncode(options?: RequestOptions): string {
+    if (!options) {
+      return "";
+    }
+
+    const parts = [];
+    for (const [key, value] of Object.entries(options)) {
+      parts.push(`${key}=${encodeURIComponent(value)}`);
+    }
+    return `?${parts.join("&")}`;
+  }
+
+  makeRequest(endpoint: string, options?: RequestOptions): Promise<Response> {
+    const params = this.urlEncode(options);
+    const url = `${CFB_ENDPOINT}${endpoint}${params}`;
+    return fetch(url, {
+      headers: {
+        Authorization: this.auth,
+        Accept: "application/json",
+      },
+    }).then((resp) => resp.json());
+  }
+}
