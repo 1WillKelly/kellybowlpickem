86fac50d839a877509b851b632f13eb8aeda22e2
Aiden Scandella <sc@ndella.com>
2022-12-10 11:34:23 -0800

    Add syncing games from CFBD and add GitHub action (#6)
    * Add sync webhook auth
    
    * Start on prisma schema
    
    * More sync
    
    * Finish sync and regen migration

--- src/pages/api/examples.ts
+++ src/pages/api/examples.ts
@@ -1,15 +1,8 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
-import { CFBDataSource } from "../../server/datasources/college-football-data-api";
-
-const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  const client = new CFBDataSource();
-  const huskyStats = await client.makeRequest("/stats/season", {
-    year: 2022,
-    team: "Washington",
-  });
+const examples = async (_req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({
-    huskyStats,
+    ok: true,
   });
 };
 
---
+++ src/server/sync/season.ts
@@ -0,0 +1,75 @@
+import { type FootballTeam, type Season } from "@prisma/client";
+import { CFBDataSource } from "server/datasources/college-football-data-api";
+import { prisma } from "server/db/client";
+
+const currentSeasonYear = (): number => {
+  const now = new Date();
+  // If it's before October, assume we're talking about the previous year's
+  // season
+  if (now.getMonth() < 10) {
+    return now.getFullYear() - 1;
+  }
+  return now.getFullYear();
+};
+
+export const getSeason = async (): Promise<Season> => {
+  const year = currentSeasonYear();
+  return await prisma.season.upsert({
+    where: {
+      year,
+    },
+    create: {
+      displayName: `${year}-${year + 1}`,
+      year,
+    },
+    update: {},
+  });
+};
+
+const upsertTeam = async (
+  apiId: string,
+  name: string
+): Promise<FootballTeam> => {
+  return await prisma.footballTeam.upsert({
+    where: {
+      apiId,
+    },
+    create: {
+      apiId,
+      name,
+    },
+    update: {
+      name,
+    },
+  });
+};
+
+export const syncBowlGames = async () => {
+  const season = await getSeason();
+  const client = new CFBDataSource();
+  const games = await client.postSeasonGames(season.year);
+  for (const game of games) {
+    const homeTeam = await upsertTeam(game.home_id.toString(), game.home_team);
+    const awayTeam = await upsertTeam(game.away_id.toString(), game.away_team);
+
+    const startDate = new Date(game.start_date);
+    const apiId = game.id.toString();
+    await prisma.footballMatchup.upsert({
+      where: {
+        apiId,
+      },
+      create: {
+        startDate,
+        apiId,
+        week: game.week,
+        seasonType: game.season_type,
+        seasonId: season.id,
+        homeTeamId: homeTeam.id,
+        awayTeamId: awayTeam.id,
+      },
+      update: {
+        startDate,
+      },
+    });
+  }
+};
