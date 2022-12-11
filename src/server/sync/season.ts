27385be55c14dc22acf7fe5caf8db751c29fba42
Aiden Scandella <sc@ndella.com>
2022-12-11 11:38:14 -0800

    Improve performance of game/team sync (#11)
    * Use batch for updating/creating teams
    
    * Sync in one transaction

--- src/server/sync/season.ts
+++ src/server/sync/season.ts
@@ -1,6 +1,10 @@
-import { type FootballTeam, type Season } from "@prisma/client";
-import { CFBDataSource } from "server/datasources/college-football-data-api";
+import { type Season } from "@prisma/client";
+import {
+  CFBDataSource,
+  type CFBDGame,
+} from "server/datasources/college-football-data-api";
 import { prisma } from "server/db/client";
+import { syncTeams } from "./teams";
 
 const currentSeasonYear = (): number => {
   const now = new Date();
@@ -26,67 +30,91 @@ export const getSeason = async (): Promise<Season> => {
   });
 };
 
-const upsertTeam = async (
-  apiId: string,
-  name: string,
-  conference: string | undefined
-): Promise<FootballTeam> => {
-  return await prisma.footballTeam.upsert({
-    where: {
-      apiId,
-    },
-    create: {
-      apiId,
-      name,
-      conference,
-    },
-    update: {
-      name,
-      conference,
-    },
-  });
-};
-
 export const syncBowlGames = async () => {
   const season = await getSeason();
   const client = new CFBDataSource();
   const games = await client.postSeasonGames(season.year);
-  for (const game of games) {
-    const homeTeam = await upsertTeam(
-      game.home_id.toString(),
-      game.home_team,
-      game.home_conference
-    );
-    const awayTeam = await upsertTeam(
-      game.away_id.toString(),
-      game.away_team,
-      game.away_conference
-    );
+  const teams = await syncTeams(games);
 
-    const startDate = new Date(game.start_date);
-    const apiId = game.id.toString();
-    const commonUpdateFields = {
-      startDate,
-      completed: game.completed,
-      homeScore: game.home_points,
-      awayScore: game.away_points,
-      name: game.notes,
+  const teamIdsToApiIds: Record<string, string> = {};
+  for (const team of teams) {
+    teamIdsToApiIds[team.apiId] = team.id;
+  }
+
+  const getTeamId = (apiId: number): string => {
+    const teamId = teamIdsToApiIds[apiId.toString()];
+    if (!teamId) {
+      throw new Error(`Could not find team with apiId ${apiId}`);
+    }
+    return teamId;
+  };
+
+  const apiGameIds = games.map((g) => g.id.toString());
+  const existingMatchups = await prisma.footballMatchup.findMany({
+    where: {
+      apiId: {
+        in: apiGameIds,
+      },
+    },
+  });
+
+  const existingMatchupIdSet = new Set(existingMatchups.map((m) => m.apiId));
+  const matchupCreateFields = (m: CFBDGame) => ({
+    apiId: m.id.toString(),
+    startDate: new Date(m.start_date),
+    completed: m.completed,
+    homeScore: m.home_points,
+    awayScore: m.away_points,
+    name: m.notes,
+    week: m.week,
+    seasonType: m.season_type,
+    seasonId: season.id,
+    homeTeamId: getTeamId(m.home_id),
+    awayTeamId: getTeamId(m.away_id),
+  });
+
+  const matchupsToCreate = games
+    .filter((m) => !existingMatchupIdSet.has(m.id.toString()))
+    .map(matchupCreateFields);
+
+  await prisma.footballMatchup.createMany({
+    data: matchupsToCreate,
+  });
+
+  const upserts = [];
+  for (const matchup of existingMatchups) {
+    const matchupData = games.find((m) => m.id.toString() === matchup.apiId);
+    if (!matchupData) {
+      throw new Error(`Could not find matchup ${matchup.apiId} in games`);
+    }
+
+    const updateFields = {
+      startDate: new Date(matchupData.start_date),
+      completed: matchupData.completed,
+      homeScore: matchupData.home_points,
+      awayScore: matchupData.away_points,
+      name: matchupData.notes,
     };
 
-    await prisma.footballMatchup.upsert({
-      where: {
-        apiId,
-      },
-      create: {
-        apiId,
-        week: game.week,
-        seasonType: game.season_type,
-        seasonId: season.id,
-        homeTeamId: homeTeam.id,
-        awayTeamId: awayTeam.id,
-        ...commonUpdateFields,
-      },
-      update: commonUpdateFields,
-    });
+    if (
+      matchup.startDate.getTime() !== updateFields.startDate.getTime() ||
+      matchup.completed !== updateFields.completed ||
+      matchup.homeScore !== updateFields.homeScore ||
+      matchup.awayScore !== updateFields.awayScore ||
+      matchup.name !== updateFields.name
+    ) {
+      upserts.push(
+        prisma.footballMatchup.update({
+          where: {
+            id: matchup.id,
+          },
+          data: updateFields,
+        })
+      );
+    }
+  }
+
+  if (upserts.length) {
+    await prisma.$transaction(upserts);
   }
 };
