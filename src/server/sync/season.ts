b530516eabd960725ad95efde087704d054d7f79
Aiden Scandella <git@sca.ndella.com>
2022-12-11 11:37:17 -0800

    Sync in one transaction

--- src/server/sync/season.ts
+++ src/server/sync/season.ts
@@ -1,5 +1,8 @@
-import { type FootballTeam, type Season } from "@prisma/client";
-import { CFBDataSource } from "server/datasources/college-football-data-api";
+import { type Season } from "@prisma/client";
+import {
+  CFBDataSource,
+  type CFBDGame,
+} from "server/datasources/college-football-data-api";
 import { prisma } from "server/db/client";
 import { syncTeams } from "./teams";
 
@@ -38,34 +41,80 @@ export const syncBowlGames = async () => {
     teamIdsToApiIds[team.apiId] = team.id;
   }
 
-  for (const game of games) {
-    const homeTeamId = teamIdsToApiIds[game.home_id.toString()];
-    const awayTeamId = teamIdsToApiIds[game.away_id.toString()];
+  const getTeamId = (apiId: number): string => {
+    const teamId = teamIdsToApiIds[apiId.toString()];
+    if (!teamId) {
+      throw new Error(`Could not find team with apiId ${apiId}`);
+    }
+    return teamId;
+  };
 
-    const startDate = new Date(game.start_date);
-    const apiId = game.id.toString();
-    const commonUpdateFields = {
-      startDate,
-      completed: game.completed,
-      homeScore: game.home_points,
-      awayScore: game.away_points,
-      name: game.notes,
+  const apiGameIds = games.map((g) => g.id.toString());
+  const existingMatchups = await prisma.footballMatchup.findMany({
+    where: {
+      apiId: {
+        in: apiGameIds,
+      },
+    },
+  });
+
+  const existingMatchupIdSet = new Set(existingMatchups.map((m) => m.apiId));
+  const matchupCreateFields = (m: CFBDGame) => ({
+    apiId: m.id.toString(),
+    startDate: new Date(m.start_date),
+    completed: m.completed,
+    homeScore: m.home_points,
+    awayScore: m.away_points,
+    name: m.notes,
+    week: m.week,
+    seasonType: m.season_type,
+    seasonId: season.id,
+    homeTeamId: getTeamId(m.home_id),
+    awayTeamId: getTeamId(m.away_id),
+  });
+
+  const matchupsToCreate = games
+    .filter((m) => !existingMatchupIdSet.has(m.id.toString()))
+    .map(matchupCreateFields);
+
+  await prisma.footballMatchup.createMany({
+    data: matchupsToCreate,
+  });
+
+  const upserts = [];
+  for (const matchup of existingMatchups) {
+    const matchupData = games.find((m) => m.id.toString() === matchup.apiId);
+    if (!matchupData) {
+      throw new Error(`Could not find matchup ${matchup.apiId} in games`);
+    }
+
+    const updateFields = {
+      startDate: new Date(matchupData.start_date),
+      completed: matchupData.completed,
+      homeScore: matchupData.home_points,
+      awayScore: matchupData.away_points,
+      name: matchupData.notes,
     };
 
-    await prisma.footballMatchup.upsert({
-      where: {
-        apiId,
-      },
-      create: {
-        apiId,
-        week: game.week,
-        seasonType: game.season_type,
-        seasonId: season.id,
-        homeTeamId,
-        awayTeamId,
-        ...commonUpdateFields,
-      },
-      update: commonUpdateFields,
-    });
+    if (
+      matchup.startDate.getTime() !== updateFields.startDate.getTime() ||
+      matchup.completed !== updateFields.completed ||
+      matchup.homeScore !== updateFields.homeScore ||
+      matchup.awayScore !== updateFields.awayScore ||
+      matchup.name !== updateFields.name
+    ) {
+      upserts.push(
+        prisma.footballMatchup.update({
+          where: {
+            id: matchup.id,
+          },
+          data: updateFields,
+        })
+      );
+    }
+  }
+
+  if (upserts.length) {
+    await prisma.$transaction(upserts);
   }
 };
