5723d55409caba4dd6d842d7d015e4dce1e19fc1
Aiden Scandella <git@sca.ndella.com>
2022-12-12 13:16:20 -0800

    Use season

--- src/server/sync/scores.ts
+++ src/server/sync/scores.ts
@@ -1,12 +1,15 @@
 import { prisma } from "server/db/client";
+import { getSeason } from "./season";
 
 export const syncScores = async () => {
+  const season = await getSeason();
   const now = new Date();
   const matchupsToSync = await prisma?.footballMatchup.findMany({
     where: {
       startDate: {
         lte: now,
       },
+      season,
       completed: false,
     },
   });
