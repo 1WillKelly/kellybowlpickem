aa167662a8c129f2595ca97c78a4e145cbf4ae22
Aiden Scandella <git@sca.ndella.com>
2022-12-17 11:34:04 -0800

    Add padding on big logo header for mobile

--- src/components/BigLogoHeader.tsx
+++ src/components/BigLogoHeader.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import BigLogo from "../assets/images/bowlpickem-logo.svg";
 
 const BigLogoHeader: React.FC = () => (
-  <section className="flex w-full items-center justify-center py-14">
+  <section className="flex w-full items-center justify-center py-14 px-4">
     <Image
       src={BigLogo.src}
       alt="Kelly Bowl Pick'em"
