cee02a28bf599bf6a41dfd78d64bb9607e42b2a8
Aiden Scandella <git@sca.ndella.com>
2022-12-26 15:05:22 -0800

    Improve date formatting

--- src/components/date-time.ts
+++ src/components/date-time.ts
@@ -1,9 +1,15 @@
 export const formatTime = (date: Date): string => {
-  return date
-    .toLocaleTimeString("en-US", {
-      weekday: "short",
-      month: "short",
-      day: "numeric",
-    })
-    .replace(":00 ", " ");
+  return (
+    date
+      .toLocaleTimeString("en-US", {
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+      })
+      .replace(":00 ", " ")
+      // Desktop
+      .replace(/([0-9]), /, "$1 - ")
+      // Mobile Safari
+      .replace(" at ", " - ")
+  );
 };
