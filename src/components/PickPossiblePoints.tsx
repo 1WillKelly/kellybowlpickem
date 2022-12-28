b9a29edb93b301253c5eb170b619d8ee7edb20e4
Aiden Scandella <git@sca.ndella.com>
2022-12-27 17:25:11 -0800

    Throw error if pick is malformed

--- src/components/PickPossiblePoints.tsx
+++ src/components/PickPossiblePoints.tsx
@@ -4,11 +4,18 @@ interface PickPossiblePointsProps {
   pick: PickWithMatchup;
 }
 
+const calculatePickPoints = (pick: PickWithMatchup): number | null => {
+  if (pick.teamId == pick.matchup.homeTeamId) {
+    return pick.matchup.homePointValue;
+  }
+  if (pick.teamId == pick.matchup.awayTeamId) {
+    return pick.matchup.awayPointValue;
+  }
+  throw new Error("Pick not for a valid team");
+};
+
 const PickPossiblePoints: React.FC<PickPossiblePointsProps> = (props) => {
-  const possiblePoints =
-    props.pick.teamId === props.pick.matchup.homeTeamId
-      ? props.pick.matchup.homePointValue
-      : props.pick.matchup.awayPointValue;
+  const possiblePoints = calculatePickPoints(props.pick);
 
   if (!possiblePoints) {
     return null;
