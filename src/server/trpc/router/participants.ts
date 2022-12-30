e3280f1c3f420fdcda8aecf09db4ba0e98f69269
Will Kelly <will@will-kelly.com>
2022-12-29 17:41:24 -0800

    Dawgfathers page - thanks AI

--- src/server/trpc/router/participants.ts
+++ src/server/trpc/router/participants.ts
@@ -1,64 +1,82 @@
 import { router, publicProcedure } from "../trpc";
 import { getSeason } from "server/sync/season";
+import { z } from "zod";
 
 export const participantsRouter = router({
-  participantsWithScores: publicProcedure.query(async ({ ctx }) => {
-    const season = await getSeason();
-    const upcomingGames = await ctx.prisma.footballMatchup.findMany({
-      where: {
-        season,
-        completed: false,
-      },
-      take: 3,
-      orderBy: {
-        startDate: "asc",
-      },
-    });
-    const participants = await ctx.prisma.participant.findMany({
-      include: {
-        seasonScores: {
-          where: {
-            season,
-          },
-        },
-        picks: {
-          where: {
-            matchupId: {
-              in: upcomingGames.map((m) => m.id),
-            },
-          },
-          orderBy: {
-            matchup: {
-              startDate: "asc",
-            },
-          },
-          include: {
-            team: {
-              select: {
-                name: true,
-                logo: true,
+  participantsWithScores: publicProcedure
+    .input(
+      z.object({
+        participantIds: z.string().array().optional(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const whereClause = input.participantIds
+        ? {
+            where: {
+              id: {
+                in: input.participantIds,
               },
             },
-            matchup: {
-              select: {
-                startDate: true,
-                homeTeamId: true,
-                awayTeamId: true,
-                homePointValue: true,
-                awayPointValue: true,
+          }
+        : undefined;
+
+      const season = await getSeason();
+      const upcomingGames = await ctx.prisma.footballMatchup.findMany({
+        where: {
+          season,
+          completed: false,
+        },
+        take: 3,
+        orderBy: {
+          startDate: "asc",
+        },
+      });
+      const participants = await ctx.prisma.participant.findMany({
+        ...whereClause,
+        include: {
+          seasonScores: {
+            where: {
+              season,
+            },
+          },
+          picks: {
+            where: {
+              matchupId: {
+                in: upcomingGames.map((m) => m.id),
+              },
+            },
+            orderBy: {
+              matchup: {
+                startDate: "asc",
+              },
+            },
+            include: {
+              team: {
+                select: {
+                  name: true,
+                  logo: true,
+                },
+              },
+              matchup: {
+                select: {
+                  startDate: true,
+                  homeTeamId: true,
+                  awayTeamId: true,
+                  homePointValue: true,
+                  awayPointValue: true,
+                },
               },
             },
           },
         },
-      },
-    });
-    return {
-      // Strip out email so we don't leak it to the frontend
-      participants: participants.map((p) => ({
-        ...p,
-        email: undefined,
-      })),
-      upcomingGames,
-    };
-  }),
+      });
+      return {
+        // Strip out email so we don't leak it to the frontend
+        participants: participants.map((p) => ({
+          ...p,
+          email: undefined,
+        })),
+        upcomingGames,
+      };
+    }),
 });
