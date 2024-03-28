08f050eff808c06f7373c46fba881dcf139337c0
Aiden Scandella <git@sca.ndella.com>
2024-03-28 14:47:52 -0700

    Remove DEBUG

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,8 +2,6 @@
 
 if [[ "${VERCEL_ENV}" == "production" ]] ; then
   echo "Running production migrations"
-  echo "DATABASE_URL: ${DATABASE_URL}"
-  echo "MIGRATIONS_DATABASE_URL: ${MIGRATIONS_DATABASE_URL}"
   DATABASE_URL="${MIGRATIONS_DATABASE_URL}" npx prisma migrate deploy
 else
   echo "Not migrating in non-prod vercel environment: ${VERCEL_ENV}"
