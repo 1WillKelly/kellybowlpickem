149e924ff54e06d6aef8e581b6b52607f688f99b
Aiden Scandella <git@sca.ndella.com>
2024-12-21 18:37:43 -0800

    next: update to 13.5 and set sync-scores api timeout

--- src/pages/api/sync/sync-scores.ts
+++ src/pages/api/sync/sync-scores.ts
@@ -2,6 +2,8 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 import { syncScores } from "server/sync/scores";
 import { webhookAuth } from "./auth";
 
+export const maxDuration = 30; // This function can run for a maximum of 5 seconds
+
 const syncScoresEndpoint = async (
   _req: NextApiRequest,
   res: NextApiResponse
