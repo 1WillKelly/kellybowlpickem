bfb3d25f886e7df8ca741c7833e1814b6a841dad
Aiden Scandella <git@sca.ndella.com>
2024-12-30 17:46:17 -0800

    style it

--- src/components/data/PickBarChart.tsx
+++ src/components/data/PickBarChart.tsx
@@ -8,25 +8,46 @@ import {
 } from "recharts";
 import { type RouterOutputs } from "utils/trpc";
 
+type Picks = RouterOutputs["picks"]["participantPicks"]["picks"];
+
 interface Props {
-  picks: RouterOutputs["picks"]["participantPicks"]["picks"];
+  picks: Picks;
   height?: number;
-  width?: number;
 }
 
-const PickBarChart: React.FC<Props> = ({ picks, height = 60, width = 84 }) => {
+const pointsForPick = (pick: Picks[number]): number => {
+  if (pick.teamId === pick.matchup.homeTeamId) {
+    return pick.matchup.homePointValue ?? 0;
+  }
+
+  if (pick.teamId === pick.matchup.awayTeamId) {
+    return pick.matchup.awayPointValue ?? 0;
+  }
+
+  // Special case for championship
+  return 0;
+};
+
+const PickBarChart: React.FC<Props> = ({ picks, height = 80 }) => {
+  const picksWithPoints = picks.map((pick) => ({
+    id: pick.id,
+    correct: pick.correct,
+    settled: pick.settled,
+    points: pointsForPick(pick),
+  }));
   return (
     <ResponsiveContainer width="100%" height={height}>
-      <BarChart data={picks} width={width}>
+      <BarChart data={picksWithPoints} barGap={20}>
         <YAxis hide />
         <XAxis dataKey="week" hide />
-        <Bar type="natural" dataKey="settledPoints" radius={2}>
-          {picks.map((pick) => {
+        <Bar type="natural" dataKey="points" radius={2}>
+          {picksWithPoints.map((pick) => {
             const fillColor = pick.settled
               ? pick.correct
                 ? "#27ae60"
-                : "hsl(0, 100%, 50%)"
-              : "hsla(230, 7%, 84%, 0.4)";
+                : "hsla(230, 7%, 84%, 0.4)"
+              : // : "hsla(230, 7%, 84%, 0.4)";
+                "hsla(226, 7%, 84%, 1)";
             return <Cell key={pick.id} fill={fillColor} />;
           })}
         </Bar>
