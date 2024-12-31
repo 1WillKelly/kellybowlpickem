00c003052420de71f11c592fc57f4112e808628a
Aiden Scandella <git@sca.ndella.com>
2024-12-30 17:13:08 -0800

    only show 5 on mobile

--- src/components/data/StreakDots.tsx
+++ src/components/data/StreakDots.tsx
@@ -5,14 +5,17 @@ interface Props {
 }
 
 const StreakDots: React.FC<Props> = ({ picks }) => {
+  const streak = picks.slice(picks.length - 10);
+
   return (
     <div className="flex flex-shrink-0 flex-row space-x-1">
-      {picks.slice(picks.length - 10).map((pick) => {
+      {streak.map((pick, idx) => {
+        const hideOnMobile = idx < streak.length - 5 ? "hidden sm:block" : "";
         const color = pick.correct ? "bg-green-correct" : "bg-red-incorrect";
         return (
           <div
             key={pick.id}
-            className={`h-[6px] w-[6px] rounded-sm ${color}`}
+            className={`h-[6px] w-[6px] rounded-sm ${color} ${hideOnMobile}`}
           />
         );
       })}
